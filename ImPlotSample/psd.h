f009b23d192ddfb8e99bc28bafd3fd3282fb00b6
dk <daigokk@gmail.com>
2025-10-20 20:09:09 +0900

    psdを実装

---
+++ ImPlotSample/psd.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cmath>
+
+#define PI acos(-1)
+
+void psd(const double arr[], const double freq, const double dt, const int size, double* pX, double* pY) {
+    *pX = 0; *pY = 0;
+    // 掛け算
+    for (int i = 0; i < size; i++) {
+        double wt = 2 * PI * freq * dt * i;
+        *pX += arr[i] * sin(wt);
+        *pY += arr[i] * cos(wt);
+    }
+    // ローパスフィルタの代わりに平均を用いる
+    *pX /= size;
+    *pY /= size;
+}
